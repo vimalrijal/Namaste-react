41c01b965cfe6d89b6cdbb67e1834cd316490076
vimalrijalmaqro <vimal.rijal@maqro.com.au>
2023-08-10 21:12:25 +1000

    adding creating element without JSX

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import React from "react";
+import { render } from "react-dom";
 import ReactDOM from "react-dom/client";
 
 const heading = React.createElement(
-  "div",
-  { id: "outer-div" },
-  React.createElement("div", { id: "inner-div" }, [
-    React.createElement(
-      "div",
-      { id: "child1" },
-      React.createElement("div", { id: "child3" }, [
-        React.createElement("h1", { id: "h1" }, "1:h1"),
-        React.createElement("h2", { id: "h2" }, "1:h2"),
-      ])
-    ),
-    React.createElement(
-      "div",
-      { id: "child2" },
-      React.createElement("div", { id: "child4" }, [
-        React.createElement("h1", { id: "h3" }, "1:h1"),
-        React.createElement("h2", { id: "h4" }, "1:h2"),
-      ])
-    ),
-  ])
+  "h2",
+  { id: "heading2" },
+  "this is a H2 element"
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
